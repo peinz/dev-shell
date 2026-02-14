4ea8a4cf71501ec289ceacf3c32306fb12446e4f
peinz <philippHeinz66@gmail.com>
2026-02-14 09:14:50 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "dev-shell that wraps current shell";
+
+  outputs = { self }: 
+    let
+      text = /* bash */ ''
+        #!/usr/bin/env bash
+
+        # base shell path
+        shell=""
+        if [ -n "''${NIX_DEVSHELL_USER_SHELL:-}" ];
+          then shell=''$NIX_DEVSHELL_USER_SHELL
+          else shell=${pkgs.bash}/bin/bash 
+        fi
+
+        # env name
+        if [ -n "''${1:-}" ];
+          then export NIX_DEVSHELL_ENV=''$1
+          else export NIX_DEVSHELL_ENV=dev-shell
+        fi
+
+        # setup env
+        export PATH="${pkgs.lib.makeBinPath deps}:''$PATH"
+        export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath deps}:''${LD_LIBRARY_PATH:-}"
+      
+        # start shell
+        exec ''$shell
+      '';
+
+    in {
+      mkShell = { pkgs, deps }:
+        pkgs.writeShellApplication {
+          name = "dev-shell";
+          runtimeInputs = [ pkgs.bash ];
+          inherit text;
+        };
+  };
+}
+
+
+
+
+
